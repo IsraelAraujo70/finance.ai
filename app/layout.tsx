b69d140cd049abd352cf57ad620f51278ca1ee2a
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-11-06 09:55:36 -0300

    feat: Updated font to mulish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Mulish } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const mulish = Mulish({
+  subsets: ["latin-ext"],
 });
 
 export const metadata: Metadata = {
@@ -26,16 +18,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    
-      <html lang="pt-br">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}>
-          <ClerkProvider appearance={{
-            baseTheme: dark
-          }}>
-            {children}
-          </ClerkProvider>
-        </body>
+    <html lang="pt-br">
+      <body className={`${mulish.className} dark antialiased`}>
+        <ClerkProvider
+          appearance={{
+            baseTheme: dark,
+          }}
+        >
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
