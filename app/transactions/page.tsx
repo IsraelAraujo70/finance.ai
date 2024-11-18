a6e6173e2558712c590f90747d4357af80849474
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-11-18 12:03:54 -0300

    feat: ensure user basic can't add transactions over the limit

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -7,6 +7,7 @@ import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -18,14 +19,16 @@ const TransactionsPage = async () => {
       userId,
     },
   });
-
+  const canUserAddTransactions = await canUserAddTransaction();
   return (
     <>
       <Navbar></Navbar>
       <div className="space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton></AddTransactionButton>
+          <AddTransactionButton
+            userCanAddTransaction={canUserAddTransactions}
+          ></AddTransactionButton>
         </div>
         <ScrollArea className="h-[90%]">
           <DataTable
