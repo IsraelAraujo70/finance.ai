5d3351bd20271ff97472ec38c17141da658f7c41
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-11-18 10:42:29 -0300

    feat: add stripe webhook that handles subscriptions

--- app/_components/logo.tsx
+++ app/_components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <div className="mb-8 flex items-center">
-      <Image src="public/logo.svg" alt="" width={39} height={39} />
+      <Image src="/logo.svg" alt="" width={39} height={39} />
       <h1 className="ms-3 text-2xl font-bold">AgiFinance</h1>
     </div>
   );
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,16 +1,18 @@
 import Navbar from "../_components/navbar";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_components/acquire-plan";
+import { Badge } from "../_components/ui/badge";
 const SubscriptionPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
   return (
     <>
       <Navbar></Navbar>
@@ -18,7 +20,12 @@ const SubscriptionPage = async () => {
         <h1 className="text-2xl font-bold">Assinatura</h1>
         <div className="flex gap-6">
           <Card className="w-[460px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {!hasPremiumPlan && (
+                <Badge className="absolute left-4 top-11 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Básico
               </h2>
@@ -40,7 +47,12 @@ const SubscriptionPage = async () => {
             </CardContent>
           </Card>
           <Card className="w-[460px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-11 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
