e8db7c7aa577fe460ba3744982311e5e5e981aaf
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-11-19 12:44:57 -0300

    fix: typo in logo

--- app/_components/logo.tsx
+++ app/_components/logo.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className="mb-8 flex items-center">
       <Image src="/logo.svg" alt="" width={39} height={39} />
-      <h1 className="ms-3 text-2xl font-bold">AgiFinance</h1>
+      <h1 className="ms-3 text-2xl font-bold">agiFinance</h1>
     </div>
   );
 };
