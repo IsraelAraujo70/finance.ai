fc2cae92c4a88b3d21268c991adf558b907762b3
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-11-19 12:26:12 -0300

    fix: fixed decimal class error in console

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -89,6 +89,6 @@ export const getDashBoard = async (month: string) => {
     expensesTotal,
     typesPercentage,
     totalExpensePerCategory,
-    lastTransactions,
+    lastTransactions: JSON.parse(JSON.stringify(lastTransactions)),
   };
 };
