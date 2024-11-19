aa16b49bb92eb6753d3f1f282318b4bbe8712ea6
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-11-19 12:22:48 -0300

    fix: order of transactions in transaction page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -18,6 +18,9 @@ const TransactionsPage = async () => {
     where: {
       userId,
     },
+    orderBy: {
+      date: "desc",
+    },
   });
   const canUserAddTransactions = await canUserAddTransaction();
   return (
